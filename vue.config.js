d1f4bfe28d5e7b52db7b89351ecde729094b750d
lihui41 <lihui41@58.com>
2019-10-16 11:29:15 +0800

    修改项目目标路径

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, './doc'),
+  outputDir: path.resolve(__dirname, './docs'),
   
   chainWebpack: config => {
     
